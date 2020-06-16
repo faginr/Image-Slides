7e41a74e62ac9f87fbcce58ce7dd54257b2dc2e9
faginr <rexwfagin@gmail.com>
2020-06-16 11:30:24 -0700

    Working Version

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // set slide index
 let slideIndex = 0;
 // define all slides
-
+let slides = document.querySelectorAll(".slide");
 // display current slide
 function displaySlide(index){
-
+    // hide other slides
+    for (let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[index].style.display = "block";
 }
